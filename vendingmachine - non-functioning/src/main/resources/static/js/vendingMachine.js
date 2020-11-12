59d21b94ce75295b4b5f0f989514be9d51c22819
Mario Valadez <mario.valadez@trustvesta.com>
2020-11-12 17:58:40 -0600

    Add issues

---
+++ vendingmachine - non-functioning/src/main/resources/static/js/vendingMachine.js
@@ -0,0 +1,81 @@
+
+function addDenomination()
+{
+	$.ajax({
+		url : "/add-to-balance",
+		data: {"denomination" : $("#denomination").val()},
+		dataType: "json"
+	}) .done(function( data ) {
+	    var response =data;
+	    if(response.status == "success")
+    	{
+	    	$("#cashBalance").html(response.cashBalance);
+	    	$("#alertContainer").css("display","none");
+    	}
+	    else{
+	    	$("#alertContainer").css("display","");
+	    	$("#alertText").html(response.error);
+	    	$("#alertText").attr("class", "alert alert-danger");
+	    }
+	}).fail(function(data) {
+	    alert( "Unexpected error: " + JSON.parse(data) );
+	});
+}
+
+function selectProduct()
+{
+	$.ajax({
+		url : "/select-product",
+		data: {"productCode" : $("#productCode").val()},
+		dataType: "json"
+	}) .done(function( data ) {
+	    var response =data;
+	    if(response.status == "success")
+    	{
+	    	$("#cashBalance").html(response.cashBalance);
+	    	
+	    	$("#alertContainer").css("display","");
+	    	$("#alertText").html(response.message);
+	    	$("#alertText").attr("class", "alert alert-success");
+	    	
+	    	if(response.cashBalance > 0 )
+    		{
+	    		$("#modalKeepBuyingBody").html(response.message + ", would like to purchase anything else?");
+	    		$('#modalKeepBuying').modal('show');
+    		}
+	    	
+    	}
+	    else{
+	    	$("#alertContainer").css("display","");
+	    	$("#alertText").html(response.error);
+	    	$("#alertText").attr("class", "alert alert-danger");
+	    }
+	}).fail(function(data) {
+	    alert( "Unexpected error: " + JSON.parse(data) );
+	});
+}
+
+function endTransaction()
+{
+	$.ajax({
+		url : "/end-transaction",
+		dataType: "json"
+	}) .done(function( data ) {
+	    var response =data;
+	    if(response.status == "success")
+    	{
+	    	$("#cashBalance").html(response.cashBalance);
+	    	
+	    	$("#alertContainer").css("display","");
+	    	$("#alertText").html(response.message);
+	    	$("#alertText").attr("class", "alert alert-success");
+    	}
+	    else{
+	    	$("#alertContainer").css("display","");
+	    	$("#alertText").html(response.error);
+	    	$("#alertText").attr("class", "alert alert-danger");
+	    }
+	}).fail(function(data) {
+	    alert( "Unexpected error: " + JSON.parse(data) );
+	});
+}
